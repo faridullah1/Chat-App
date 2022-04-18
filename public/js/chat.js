0a68ac3041ec4fc5bb0c45c32882779f01336687
Farid ullah <faridullah996@gmail.com>
2022-04-18 12:34:29 +0500

    dynamic rendering of messages with time

--- public/js/chat.js
+++ public/js/chat.js
@@ -1,24 +1,43 @@
 const socket = io();
 
-socket.on('message', (message) => {
-	console.log('new Message =>', message);
-});
-
 // Elements
 const $sendBtn = document.querySelector('#btnSend');
 const $sendLocationBtn = document.querySelector('#btnSendLocation');
-const $messageContainer = document.querySelector('#message');
+const $messageField = document.querySelector('#messageField');
+const $messages = document.querySelector('#messages');
+
+// Templates
+const messageTemplate = document.querySelector('#messageTemplate').innerHTML;
+const locationTemplate = document.querySelector('#locationTemplate').innerHTML;
+
+socket.on('message', (message) => {
+	console.log('new Message =>', message);
+	const html = Mustache.render(messageTemplate, {
+		message: message.text,
+		createdAt: moment(message.createdAt).format('h:mm a')
+	});
+	$messages.insertAdjacentHTML('beforeend', html);
+});
+
+socket.on('locationMessage', (location) => {
+	console.log('location =', location);
+	const html = Mustache.render(locationTemplate, {
+		url: location.url,
+		createdAt: moment(location.createdAt).format('h:mm a')
+	});
+	$messages.insertAdjacentHTML('beforeend', html);
+})
 
 // Click event listeners;
 $sendBtn.addEventListener('click', () => {
 	$sendBtn.setAttribute('disabled', 'disabled');
 
-	const message = $messageContainer.value;
+	const message = $messageField.value;
 
 	socket.emit('sendMessage', message, (error) => {
 		$sendBtn.removeAttribute('disabled');
-		$messageContainer.value = '';
-		$messageContainer.focus();
+		$messageField.value = '';
+		$messageField.focus();
 
 		if (error) return console.error(error);
 
